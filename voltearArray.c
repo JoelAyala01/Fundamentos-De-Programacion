2db7bcb44ad5e9310edff203fe8a4610cf8c15eb
Joel Ayala <140276353+JoelAyala01@users.noreply.github.com>
2025-05-25 15:52:24 -0400

    Create voltearArray.c

---
+++ voltearArray.c
@@ -0,0 +1,9 @@
+void voltear(int *arr, int n){
+    int *temp;
+    for(int i=0; i<n/2; i++)
+    {
+        *temp = *(arr+i);
+        *(arr+i) = *(arr + n-i-1);
+        *(arr + n-i-1) = *temp;
+    }
+}
