7945fb27e10b0d9e8994ba2b2d46a00e0f1d61f7
Joel Ayala <140276353+JoelAyala01@users.noreply.github.com>
2025-11-03 18:14:24 -0400

    Enhance student management with dynamic reallocation
    Refactor student management program to handle dynamic allocation and addition of new students.

--- Parcial 2 - Intento 3.c
+++ Parcial 2 - Intento 3.c
@@ -9,8 +9,8 @@ typedef struct
     float promedio;
 } ESTUDIANTE;
 
-void llenarEstud(ESTUDIANTE *, int);
-void mostrarEstud(ESTUDIANTE *, int);
+void llenarEstud(ESTUDIANTE *, int, int);
+void mostrarEstud(ESTUDIANTE *, int, int);
 float promGeneral_rec(ESTUDIANTE *, int);
 float notaMayor_rec(ESTUDIANTE *, int);
 
@@ -20,13 +20,51 @@ int main()
     printf("Estudiantes a registrar: ");
     scanf("%d",&n);
     ESTUDIANTE *e = (ESTUDIANTE *)malloc(n*sizeof(ESTUDIANTE));
-    llenarEstud(e,n);
+    if(e==NULL)
+    {
+        printf("\n\tError al asignar memoria!\a\n");
+        return 1;
+    }
+    llenarEstud(e,0,n);
     system("CLS");
-    mostrarEstud(e,n);
+    mostrarEstud(e,0,n);
+    printf("\n\n");
+    system("PAUSE");
+
     float promFinal = (promGeneral_rec(e,n)/n);
     printf("\n\nPromedio Final General: %.3f", promFinal);
     printf("\nNota mas alta: %.3f\n\n",notaMayor_rec(e,n));
 
+    system("CLS");
+
+    int nuevos;
+    int total, totalviejo;
+    char opcion;
+
+    do{
+    printf("\nDesea agregar mas estudiantes: (S/N)");
+    scanf(" %c",&opcion);
+    if(opcion == 'S' || opcion == 's')
+    {
+        printf("\nCuantos nuevos agregaras? ");
+        scanf("%d",&nuevos);
+
+        totalviejo = n;
+        total = n+nuevos;
+        e = (ESTUDIANTE *)realloc(e,total*sizeof(ESTUDIANTE));
+        if(e==NULL)
+        {
+            printf("\n\tError de memoria!\a\n");
+            return 1;
+        }
+
+        llenarEstud(e,totalviejo,total);
+        mostrarEstud(e,0,total);
+        n = total;
+    }
+    }while(opcion == 'S' || opcion == 's');
+
+    free(e);
     system("PAUSE");
     return 0;
 }
@@ -58,9 +96,9 @@ float promGeneral_rec(ESTUDIANTE *e, int n)
     }
 }
 
-void llenarEstud(ESTUDIANTE *e, int n)
+void llenarEstud(ESTUDIANTE *e, int inicio, int fin)
 {
-    for(int i=0; i<n; i++)
+    for(int i=inicio; i<fin; i++)
     {
         printf("\nEstudiante #%d: \n",i+1);
         getchar();
@@ -74,11 +112,11 @@ void llenarEstud(ESTUDIANTE *e, int n)
     }
 }
 
-void mostrarEstud(ESTUDIANTE *e, int n)
+void mostrarEstud(ESTUDIANTE *e, int inicio, int fin)
 {
     printf("=== Datos de los estudiantes ===\n\n");
     printf("\n%-20s %-5s %10s \n","Nombre: ", "Edad: ", "Promedio: ");
-    for(int i=0; i<n; i++)
+    for(int i=inicio; i<fin; i++)
     {
         printf("\n%-20s %5d %9.2f", (e+i)->nombre, (e+i)->edad, (e+i)->promedio);
     }
